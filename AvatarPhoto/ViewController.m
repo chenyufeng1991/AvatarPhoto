988e6a5ffcec35f12f24ff4c7b0e9205ab4066c3
chenyufeng <503434355@qq.com>
2015-12-12 22:34:05 +0800

    chose the style of photo

--- AvatarPhoto/ViewController.m
+++ AvatarPhoto/ViewController.m
@@ -23,17 +23,20 @@
 - (void)viewDidLoad {
   [super viewDidLoad];
 
+
+
+  NSLog(@"viewDidLoad");
   //设置圆形头像；
   [self setCirclePhoto];
-  
-  
+
+
 }
 
 -(void)viewWillAppear:(BOOL)animated
 {
   [super viewWillAppear:animated];
-  
-  
+
+
   UIImage *image = [[MyImageStore sharedStore] imageForKey:@"CYFStore"];
   self.avatarImage.image = image;
 }
@@ -43,49 +46,103 @@
 
   [self.avatarImage.layer setCornerRadius:CGRectGetHeight([self.avatarImage bounds]) / 2];
   self.avatarImage.layer.masksToBounds = true;
-  
+
   //可以根据需求设置边框宽度、颜色
   self.avatarImage.layer.borderWidth = 1;
   self.avatarImage.layer.borderColor = [[UIColor blackColor] CGColor];
   //设置图片；
   self.avatarImage.layer.contents = (id)[[UIImage imageNamed:@"avatar.png"] CGImage];
-  
-  
+
+
 }
 
 - (IBAction)selectPhoto:(id)sender {
-  
-  
+
+
   if ([self.imagePickerPopover isPopoverVisible]) {
     [self.imagePickerPopover dismissPopoverAnimated:YES];
     self.imagePickerPopover = nil;
     return;
   }
-  
+
   UIImagePickerController *imagePicker = [[UIImagePickerController alloc] init];
   imagePicker.editing = YES;
-  
-  //如果设备支持相机，就使用拍照技术
-  //否则让用户从照片库中选择照片
-  if([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera])
-  {
-    imagePicker.sourceType = UIImagePickerControllerSourceTypeCamera;
-  }
-  else{
-    imagePicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
-  }
-  
   imagePicker.delegate = self;
-  
-  //允许编辑图片
-  imagePicker.allowsEditing = YES;
 
   /*
    如果这里allowsEditing设置为false，则下面的UIImage *image = [info valueForKey:UIImagePickerControllerEditedImage];
    应该改为： UIImage *image = [info valueForKey:UIImagePickerControllerOriginalImage];
    也就是改为原图像，而不是编辑后的图像。
    */
-  
+
+  //允许编辑图片
+  imagePicker.allowsEditing = YES;
+
+
+  //如果设备支持相机，就使用拍照技术
+  //否则让用户从照片库中选择照片
+  //  if([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera])
+  //  {
+  //    imagePicker.sourceType = UIImagePickerControllerSourceTypeCamera;
+  //  }
+  //  else{
+  //    imagePicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+  //  }
+
+  /*
+
+   这里以弹出选择框的形式让用户选择是打开照相机还是图库
+
+   */
+
+  //初始化提示框；
+  UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"请选择打开方式" message:nil preferredStyle:  UIAlertControllerStyleActionSheet];
+
+  [alert addAction:[UIAlertAction actionWithTitle:@"照相机" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+
+    imagePicker.sourceType = UIImagePickerControllerSourceTypeCamera;
+
+    //创建UIPopoverController对象前先检查当前设备是不是ipad
+    if ([UIDevice currentDevice].userInterfaceIdiom == UIUserInterfaceIdiomPad) {
+      self.imagePickerPopover = [[UIPopoverController alloc] initWithContentViewController:imagePicker];
+      self.imagePickerPopover.delegate = self;
+      [self.imagePickerPopover presentPopoverFromBarButtonItem:sender
+                                      permittedArrowDirections:UIPopoverArrowDirectionAny
+                                                      animated:YES];
+    }
+    else{
+      [self presentViewController:imagePicker animated:YES completion:nil];
+
+    }
+  }]];
+
+  [alert addAction:[UIAlertAction actionWithTitle:@"相册" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+
+    imagePicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+
+    //创建UIPopoverController对象前先检查当前设备是不是ipad
+    if ([UIDevice currentDevice].userInterfaceIdiom == UIUserInterfaceIdiomPad) {
+      self.imagePickerPopover = [[UIPopoverController alloc] initWithContentViewController:imagePicker];
+      self.imagePickerPopover.delegate = self;
+      [self.imagePickerPopover presentPopoverFromBarButtonItem:sender
+                                      permittedArrowDirections:UIPopoverArrowDirectionAny
+                                                      animated:YES];
+    }
+    else{
+      [self presentViewController:imagePicker animated:YES completion:nil];
+    }
+  }]];
+
+  [alert addAction:[UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleDestructive handler:^(UIAlertAction * _Nonnull action) {
+    //取消；
+  }]];
+
+  //弹出提示框；
+  [self presentViewController:alert animated:true completion:nil];
+
+
+
+
   //创建UIPopoverController对象前先检查当前设备是不是ipad
   if ([UIDevice currentDevice].userInterfaceIdiom == UIUserInterfaceIdiomPad) {
     self.imagePickerPopover = [[UIPopoverController alloc] initWithContentViewController:imagePicker];
@@ -105,19 +162,19 @@
   //通过info字典获取选择的照片
   UIImage *image = [info valueForKey:UIImagePickerControllerEditedImage];
 
-  
+
   //以itemKey为键，将照片存入ImageStore对象中
   [[MyImageStore sharedStore] setImage:image forKey:@"CYFStore"];
-  
+
   //将照片放入UIImageView对象
   self.avatarImage.image = image;
 
 
   //把一张照片保存到图库中，此时无论是这张照片是照相机拍的还是本身从图库中取出的，都会保存到图库中；
-   UIImageWriteToSavedPhotosAlbum(image, self, nil, nil);
+  UIImageWriteToSavedPhotosAlbum(image, self, nil, nil);
 
   //压缩图片,如果图片要上传到服务器或者网络，则需要执行该步骤（压缩），第二个参数是压缩比例，转化为NSData类型；
-    NSData *fileData = UIImageJPEGRepresentation(image, 1.0);
+  NSData *fileData = UIImageJPEGRepresentation(image, 1.0);
 
 
   //判断UIPopoverController对象是否存在
